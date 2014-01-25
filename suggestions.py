4d922189a559950641897e49548ad9368dd5048d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-25 20:41:35 +0100

    Algo for suggestions

--- suggestions.py
+++ suggestions.py
@@ -1,14 +1,26 @@
-import itertools as it
-from database.models import Client
+from database.models import Client, Product
 
 def get_client_similarities(client):
     a1 = client.boolean_attributes
-    for other_client in Client.objects.filter(id__ne=client.id)
+    for other_client in Client.objects(contact_id__ne=client.contact_id):
         a2 = other_client.boolean_attributes
-        score = sum(it.imap(lambda x: float((x[0]+1)*x[0]), it.compress(a1, a2)))
-        score /= len(a2)*(len(a2)+1)/2
-        yield other_client, score
+        yield other_client, map(lambda x: x[0] == x[1], zip(a1, a2)).count(True)
+
+def get_client_estimated_score(client, product):
+    sum_ = 0
+    client_feedbacks = client.reunions.feedbacks
+    for feedback in client_feedbacks:
+        if product not in feedback:
+            continue
+        sum_ += 1 if feedback.positive else -1
+    sum_ /= len(client_feedbacks)
+    return sum_
 
 def get_suggested_products(client):
     client_similarities = get_client_similarities(client)
-    pass # TODO
+    for product in Product.objects:
+        client_sum = 0
+        for other_client, client_score in client_similarities:
+            client_sum += get_client_estimated_score(other_client, product)
+        client_sum /= len(client_similarities)
+        yield product, client_sum
