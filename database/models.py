4c06dc47f9e133f4f3fbe1b7c88f7d3ad287f746
Adrien Duffy <adrien.duffy-coissard@insa-lyon.fr>
2014-01-25 13:09:54 +0100

    add databases generator

---
+++ database/models.py
@@ -0,0 +1,20 @@
+from mongoengine import Document, fields
+# StringField, BooleanField, ListField, IntField, ReferenceField
+
+class Contract(Document):
+    product_id = fields.StringField(unique=True)
+
+class Client(Document):
+    contact_id = fields.StringField(unique=True)
+    has_car = fields.BooleanField(default=False)
+    has_child = .BooleanField(default=False)
+    is_married = fields.BooleanField(default=False)
+    is_owner = fields.BooleanField(default=False)
+    interests = fields.ListField(Contract)
+
+class Challenge(Document):
+    title = fields.StringField()
+    contract = fields.ReferenceField(Contract)
+    current_value = fields.IntField()
+    max_value = fields.IntField()
+    description = fields.StringField()
