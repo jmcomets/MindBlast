d013e90dee61d631dbded26377f7506693093959
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-25 17:56:32 +0100

    Add random generator client data

---
+++ database/generate_clients.py
@@ -0,0 +1,23 @@
+import random
+
+probas = {
+    'has_car': 0.7,
+    'has_child': 0.45,
+    'is_married': 0.45,
+    'is_owner': 0.3,
+    }
+
+with open('contacts.txt', 'r') as fp:
+    clients = map(lambda l: {
+        'has_car': False,
+        'has_child': False,
+        'is_married': False,
+        'is_owner': False,
+        'contact_id':l.strip('\r\n')
+        }, fp.readlines())
+
+for pk, p in probas.iteritems():
+    random.shuffle(clients)
+    for i in xrange(0, int(p*len(clients))):
+        clients[i][pk] = True
+random.shuffle(clients)
