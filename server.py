0948125d235cf263fd41589abee63ab6576fb51b
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-25 02:30:07 +0100

    Add server, requirements and logo (lol)

---
+++ server.py
@@ -0,0 +1,35 @@
+import sys
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+<pre>
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+</pre>
+"""
+
+if __name__ == '__main__':
+    app.run(debug='--debug' in sys.argv, port=8888, ssl_context='adhoc')
