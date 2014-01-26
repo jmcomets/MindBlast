89b9c388dbd47f94c0b9d52190d6ff010c4914ce
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-26 08:32:34 +0100

    Complete meeting template

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import sys
-import beatbox
 from flask import Flask, render_template
 from suggestions import (get_suggested_products_jm, get_suggested_products_ahmed)
 from database import connect as connect_to_database
@@ -14,7 +13,7 @@ connect_to_database()
 def list_clients():
     return render_template('list_clients.html', clients=Client.objects)
 
-@app.route('/<client_id>')
+@app.route('/clients/<client_id>')
 def client_detail(client_id):
     client = Client.objects.get(id=client_id)
     suggestions = sorted(get_suggested_products_jm(client), key=lambda x: x[1], reverse=True)
@@ -22,15 +21,8 @@ def client_detail(client_id):
     risqued = filter(lambda x: x[1] < 0.3, suggestions)
     return render_template('client_detail.html', **locals())
 
-@app.route('/api/suggestions/<contact_id>/products')
-def suggested_products(contact_id):
-    client = Client.objects(contact_id=contact_id)
-    # TODO handle errors
-    suggestions = get_suggested_products(client)
-    return '<br>'.join(map(lambda x: x[0] + ':' + x[1], suggestions))
-
-@app.route('/<client_id>/reunion')
-def meeting():
+@app.route('/clients/reunions/<client_id>')
+def meeting(client_id):
     client = Client.objects.get(id=client_id)
     products = [s[0] for s in sorted(get_suggested_products_jm(client), key=lambda x: x[1])]
     return render_template('meeting.html', **locals())
